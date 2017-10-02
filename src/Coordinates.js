406479a2a4ec44d8702d49c8224895d74d1553e1
lducoroy <l.ducoroy@gmail.com>
2017-10-02 12:02:02 +0200

    Tableau avec 43 coordonnées valides.

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -5,8 +5,19 @@ Lyngk.Coordinates = function (c, l) {
     var ligne=l;
 
     this.valid=function() {
-        if (colonne === 'A' && ligne === 1) {
-            return false;
+        var dansTab = {
+            "A": [3, 3],
+            "B": [2, 5],
+            "C": [1, 7],
+            "D": [2, 7],
+            "E": [2, 8],
+            "F": [3, 8],
+            "G": [3, 9],
+            "H": [5, 8],
+            "I": [7, 7]
+        }
+        if(l>=dansTab[c][0] && l<=dansTab[c][1]){
+            return true;
         }
     }
 };

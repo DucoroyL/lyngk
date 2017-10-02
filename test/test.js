d48ffebaa4481813413b7885c3dce1cc4018605c
lducoroy <l.ducoroy@gmail.com>
2017-10-02 11:40:18 +0200

    Modification Test Histoire 2

--- test/test.js
+++ test/test.js
@@ -2,14 +2,15 @@
 
 var LyngkTestCase = TestCase("LyngkTestCase");
 LyngkTestCase.prototype.test1=function(){
-    var C=new Lyngk.Coordinates("A",1);
     var lettres="ABCDEFGHI";
     var compteur=0;
-    for (var i =1; i<9; i++){
+    for (var i =0; i<lettres.length; i++){
         for (var j = 1; j<9; j++){
+            var C=new Lyngk.Coordinates(i,j);
             if(assertTrue(C.valid(lettres[i],j))){
                 compteur++;
             }
         }
     }
+    assertTrue(compteur===43);
 };

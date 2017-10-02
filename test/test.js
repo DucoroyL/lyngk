7d227018271b90aa0be867715a41a8817a54e7b6
lducoroy <l.ducoroy@gmail.com>
2017-10-02 11:23:44 +0200

    Test Histoire 2

--- test/test.js
+++ test/test.js
@@ -3,5 +3,13 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 LyngkTestCase.prototype.test1=function(){
     var C=new Lyngk.Coordinates("A",1);
-    assertFalse(C.valid());
+    var lettres="ABCDEFGHI";
+    var compteur=0;
+    for (var i =1; i<9; i++){
+        for (var j = 1; j<9; j++){
+            if(assertTrue(C.valid(lettres[i],j))){
+                compteur++;
+            }
+        }
+    }
 };

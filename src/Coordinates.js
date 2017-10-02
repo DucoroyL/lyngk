844e7589a2aa9f96a3d02e3452232b7c55bcb08c
lducoroy <l.ducoroy@gmail.com>
2017-10-02 11:02:25 +0200

    Fonction valid qui check les coordonnées A1

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,12 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+    var colonne=c;
+    var ligne=l;
+
+    this.valid=function() {
+        if (colonne === 'A' && ligne === 1) {
+            return false;
+        }
+    }
 };

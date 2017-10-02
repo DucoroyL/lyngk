f4261438ae6f1d9a5f4bf4ffdbc0b6b9134c782a
lducoroy <l.ducoroy@gmail.com>
2017-10-02 12:00:53 +0200

    Modification 3 Test Histoire 2

--- test/test.js
+++ test/test.js
@@ -5,12 +5,13 @@ LyngkTestCase.prototype.test1=function(){
     var lettres="ABCDEFGHI";
     var compteur=0;
     for (var i =0; i<lettres.length; i++){
-        for (var j = 1; j<9; j++){
+        for (var j = 1; j<=9; j++){
             var C=new Lyngk.Coordinates(lettres[i],j);
-            if(assertTrue(C.valid())){
+            if(C.valid()){
                 compteur++;
             }
         }
     }
+    console.log(compteur);
     assertTrue(compteur===43);
 };

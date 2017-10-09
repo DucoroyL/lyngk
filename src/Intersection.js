375444174eb000f2ea49190f1ecae3fe853db64d
lducoroy <l.ducoroy@gmail.com>
2017-10-09 11:18:44 +0200

    7 - Mise à VACANT des intersections de départ

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,4 +3,9 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
+    var coords = c;
+    var etat = Lyngk.State.VACANT;
+    this.getState=function(){
+        return etat;
+    }
 };

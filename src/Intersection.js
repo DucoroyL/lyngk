a232c0f58481cf7ad84ad101547a110c538ed548
lducoroy <l.ducoroy@gmail.com>
2017-10-09 12:16:10 +0200

    8 - Implémentation de la couleur d'une pièce; d'une intersection; et la mise à l'état ONE_PIECE

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,10 +2,25 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function (c, color) {
     var coords = c;
     var etat = Lyngk.State.VACANT;
+    var listPiece=[];
+    var couleur=Lyngk.Color[color];
+
     this.getState=function(){
         return etat;
-    }
+    };
+
+    this.setState=function(etat_p){
+        etat = Lyngk.State[etat_p];
+    };
+
+    this.getListPiece=function (){
+        return listPiece;
+    };
+
+    this.setListPiece=function(newPiece){
+        listPiece.push(newPiece);
+    };
 };
--- src/Piece.js
+++ src/Piece.js
@@ -1,4 +1,14 @@
 "use strict";
 
-Lyngk.Piece = function (c) {
+Lyngk.Piece = function (c, newColor) {
+    var coor = c;
+    var couleur=Lyngk.Color[newColor];
+
+    this.getCouleur = function(){
+        return couleur;
+    }
+
+    this.setCouleur = function(newCouleur){
+        couleur=Lyngk.Color[newCouleur];
+    }
 };
